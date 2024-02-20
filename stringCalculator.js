e33c0f44badcf870071cf794b37ac4b03c700226
prajjwal-gavander <prajjwal.gavander.2029@gmail.com>
2024-02-21 01:31:11 +0530

    feat: modified add method for two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,9 @@
 const add = (string) => {
   if (string === "") return 0;
+  else if (string.includes(",")) {
+    const numbers = string.split(",");
+    return parseInt(numbers[0]) + parseInt(numbers[1]);
+  }
   else return parseInt(string);
 }
 
